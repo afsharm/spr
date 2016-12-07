645e25cd9bc44426a6ce3cc3fa89db322d70ead8
afshar <afshar.mohebbi@gmail.com>
2016-12-07 16:09:23 +0330

    edit club

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -12,6 +12,10 @@ class ClubsController < ApplicationController
     @club = Club.new
   end
 
+  def edit
+    @club=Club.find(params[:id])
+  end
+
   def create
       @club = Club.new(club_params)
 
@@ -19,6 +23,16 @@ class ClubsController < ApplicationController
         redirect_to @club
       else
         render 'new'
+      end
+  end
+
+  def update
+    @club = Club.find(params[:id])
+
+    if (@club.update(club_params))
+      redirect_to @club
+      else
+        render 'edit'
       end
   end
 
